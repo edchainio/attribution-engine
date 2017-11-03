33f15d080f4d5485b20df4d0d20cad63294847db
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-11-03 12:12:21 -0400

    Add content addresses

--- client/emulator.py
+++ client/emulator.py
@@ -3,4 +3,16 @@
 import json
 import requests
 
+
+# curl "http://127.0.0.1:5000/content/addresses/0"
+# curl "http://127.0.0.1:5000/content/addresses/1"
+# curl "http://127.0.0.1:5000/content/addresses/2"
+# curl "http://127.0.0.1:5000/content/addresses/3"
+# curl "http://127.0.0.1:5000/content/addresses/4"
+# curl "http://127.0.0.1:5000/content/addresses/5"
+# curl "http://127.0.0.1:5000/content/addresses/6"
+# curl "http://127.0.0.1:5000/content/addresses/7"
+# curl "http://127.0.0.1:5000/content/addresses/8"
+
+
 x = requests.get('').json()
