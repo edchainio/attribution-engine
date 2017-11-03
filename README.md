9c22c6073faf5a6d7b933e284ac793640d710a4e
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-11-03 09:54:59 -0400

    Save progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A Prototype of an Attribution Engine for edChain
 
 Install autoenv
+
+the client is really just a stand-in to emulate the functionality of the electron app (client-gui)
--- server/controller.py
+++ server/controller.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
+import json
+
 from flask import Flask
+
+
+api = Flask(__name__)
+
+@api.route('/content/addresses/<int:course_number>', methods=["GET"])
+def get_content_addresses_by_course_number(course_number):
+    with open('docstore/{}/new.json'.format(course_number), "r") as file:
+        document = json.load(file)
+        return document[0]['hash']
+
+    print(course_number)
+    pass
+
+
+if __name__ == '__main__':
+    api.run(host='127.0.0.1', port=5000, debug=True)
