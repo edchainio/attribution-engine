ec4bc218a7c3f34c4b62f5e2dafe6fe9261b5564
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-11-06 23:45:18 -0500

    Add endpoint for Featured Courses

--- server/controller.py
+++ server/controller.py
@@ -2,7 +2,7 @@
 
 import json
 
-from flask import Flask
+from flask import Flask, jsonify
 
 
 api = Flask(__name__)
@@ -13,5 +13,21 @@ def get_content_addresses_by_course_number(course_number):
         document = json.load(file)
         return document[0]['hash']
 
+@api.route('/content/addresses/featured', methods=["GET"])
+def get_content_addresses_for_featured_courses():
+#    number_of_featured_courses = 9
+#    for featured_course in number_of_featured_courses:
+#        print(featured_course)
+    print('test')
+    with open('docstore/featured/new.json', "r") as file:
+        document = json.load(file)
+        # print(json.dump(document))
+        # print(type(json.dump(document)))
+        # return json.dump(document)
+        return jsonify(document)
+
+
+
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=5000, debug=True)
+    api.run(host='127.0.0.1', port=5000, debug=True)
+#    api.run(host='0.0.0.0', port=5000, debug=True)
