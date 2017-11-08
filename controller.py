6684680c8745bae10d5e6c1a39c2fd5eb1d885b1
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-11-08 14:34:39 -0500

    Change directory name from docstore to srv

--- controller.py
+++ controller.py
@@ -10,14 +10,14 @@ api = Flask(__name__)
 # This takes a course number and returns an address for content hosted on IPFS
 @api.route('/content/addresses/<int:course_number>', methods=["GET"])
 def get_content_addresses_by_course_number(course_number):
-    with open('docstore/{}/new.json'.format(course_number), "r") as file:
+    with open('srv/{}/new.json'.format(course_number), "r") as file:
         document = json.load(file)
         return document[0]['hash']
 
 # This returns an arbitrary array of courses in lieu of a recommendation system
 @api.route('/content/addresses/featured', methods=["GET"])
 def get_content_addresses_for_featured_courses():
-    with open('docstore/featured/new.json', "r") as file:
+    with open('srv/featured/new.json', "r") as file:
         document = json.load(file)
         return jsonify(document)
 
