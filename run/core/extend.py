f246a8c465bd7971ce7e42050451f501d0054dee
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-12-26 00:12:54 -0500

    Move authorization module

--- run/core/extend.py
+++ run/core/extend.py
@@ -2,18 +2,18 @@
 
 from functools import wraps
 from flask import(
-					flash,
-					g,
-					redirect,
-					request,
-					url_for)
+                    flash,
+                    g,
+                    redirect,
+                    request,
+                    url_for)
 
 
 def authorize(endpoint):
-	@wraps(endpoint)
-		def authorizing(*args, **kwargs):
-			if g.username is None:
-				flash(u'Please log-in.', 'error')
-				return redirect(url_for('core.authenticate', next=request.path))
-			return endpoint(*args, **kwargs)
-		return authorizing
+    @wraps(endpoint)
+    def authorizing(*args, **kwargs):
+        if g.username is None:
+            flash(u'Please log-in.', 'error')
+            return redirect(url_for('core.authenticate', next=request.path))
+        return endpoint(*args, **kwargs)
+    return authorizing
