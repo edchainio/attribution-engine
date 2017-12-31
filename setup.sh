b9b4ed74783b28bc5fcbd5cbc53aeccdb5a0eb32
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-12-31 18:06:08 -0500

    Make virtualenv for python3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get -y install python3-pip virtualenv
-virtualenv --no-site-packages venv
+virtualenv -p python3 --no-site-packages venv
 source venv/bin/activate
 pip3 install -r requirements.txt
